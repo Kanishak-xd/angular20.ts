bec45bfb8de4c4deece06a997a06df448bd26808
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-06-20 08:02:12 +0900

    added control flow statement component having @if, @else if, @else and ngModel synced text and input

---
+++ src/app/components/control-flow/control-flow.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-control-flow',
+  imports: [FormsModule],
+  templateUrl: './control-flow.html',
+  styleUrl: './control-flow.css'
+})
+export class ControlFlow {
+  ngModVar: string = "Synced Text";
+}
